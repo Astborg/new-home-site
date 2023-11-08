766e91c0319f98ee202fd657d336473b9c8b85da
Astborg <93790636+Astborg@users.noreply.github.com>
2023-11-08 13:37:48 +0100

    third commit

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,68 @@ import { BlockRenderer } from "components/BlockRenderer";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
 
 export default function Home(props) {
-  console.log(props)
+  console.log(props);
   return <div><BlockRenderer blocks={props.blocks}></BlockRenderer></div>;
 }
+
 export const getStaticProps = async () => {
-  const {data} = await client.query({
-    query: gql`
-    query NewQuery {
-      nodeByUri(uri: "/") {
-        ... on Page {
-          id
-          blocks
+  try {
+    const response = await client.query({
+      query: gql`
+        query PageQuery {
+          nodeByUri(uri: "/") {
+            ... on Page {
+              id
+              blocks
+            }
+          }
+          acfOptionsMainMenu {
+            Mainmenu {
+              menuItems {
+                menuItem {
+                  destination {
+                    ...on Page {
+                      uri
+                    }
+                  }
+                  label
+                }
+                items {
+                  destination {
+                    ...on Page {
+                      uri
+                    }
+                  }
+                  label
+                }
+              }
+            }
+          }
         }
-      }
+      `
+    });
+
+    console.log('GraphQL Response:', response);
+
+    if (response.errors) {
+      console.error('GraphQL Errors:', response.errors);
+      // Handle errors, maybe set a default value for blocks or show an error message
     }
-    `
-  })
-  return {
-    props: {
-      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks),
-      
-    }
+
+    return {
+      props: {
+        mainMenuItems: response.data.acfOptionsMainMenu.mainMenu.menuItems,
+        blocks: cleanAndTransformBlocks(response.data.nodeByUri.blocks),
+      },
+    };
+  } catch (error) {
+    console.error('Error in getStaticProps:', error);
+    // Handle the error, maybe set a default value for blocks or show an error message
+    return {
+      props: {
+        mainMenuItems: [],
+        blocks: [], // Set a default value
+      },
+    };
   }
-}
+};
